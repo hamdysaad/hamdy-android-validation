dbfbc151f71454adb0594064c6f74e3444202da7
Hamdy Saad <hsaad@tacme.com>
2019-05-26 16:10:07 -0700

    init repo

---
+++ validation/src/main/java/com/hamdy/android/lib/validation/HamdyValidation.java
@@ -0,0 +1,14 @@
+package com.hamdy.android.lib.validation;
+
+import android.util.Log;
+
+public class HamdyValidation {
+
+    private static final String TAG =  "HamdyValidation";
+
+
+    public static  void  Validation(){
+
+        Log.e(TAG , "Validation");
+    }
+}
